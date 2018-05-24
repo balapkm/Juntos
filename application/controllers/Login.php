453e9188185f3f4d5705dcd643d32f6e4ab823d1
balakumaran <balakumaran.raji@gmail.com>
2018-05-24 08:26:34 +0530

    issue

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -22,12 +22,18 @@ class Login extends CI_Controller {
 		$data = $this->commonQuery->loginUser($this->data['username'],md5($this->data['password']));
 		if(count($data) == 1)
 		{
+			// server should keep session data for AT LEAST 1 hour
+			ini_set('session.gc_maxlifetime', 18000);
+
+			// each client should remember their session id for EXACTLY 1 hour
+			session_set_cookie_params(18000);
 			session_id(md5($this->data['username'].$this->data['password']));
 			session_start();
 			$_SESSION['user_group_id']   = $data[0]['user_group_id'];
 			$_SESSION['user_login_name'] = $data[0]['user_login_name'];
 			$_SESSION['user_login_id']   = $data[0]['user_login_id'];
 			$data[0]['session_id'] = session_id();
+
 			return $data[0];
 		}
 		else
