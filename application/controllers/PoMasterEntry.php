53ef513be62d3dbaec44ec90e0c0785f0a3f4d43
balakumaran <balakumaran.raji@gmail.com>
2020-02-05 20:54:47 +0530

    Log write

--- application/controllers/PoMasterEntry.php
+++ application/controllers/PoMasterEntry.php
@@ -9,12 +9,14 @@ class PoMasterEntry extends CI_Controller
 	{
 		parent::__construct();
 		$this->load->library('Mysmarty');
+		$this->load->library('Logwrite');
 		$this->load->model('PoMasterEntryQuery');
 		$this->load->config('application');
 	}
 
 	public function index()
 	{
+		//print_r($this->logwrite->logWrite());
 		$this->data['supplier_entry'] = $this->PoMasterEntryQuery->select_supplier_entry();
 		$this->data['max_supplier_id'] = $this->PoMasterEntryQuery->get_max_supplier_id();
 		$this->data['material_entry'] = $this->PoMasterEntryQuery->select_material_entry();
---
+++ application/libraries/Logwrite.php
@@ -0,0 +1,18 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Smartie Class
+ *
+ * @package        CodeIgniter
+ * @subpackage     Libraries
+ * @category       Smarty
+ * @author         Kepler Gelotte
+ * @link           http://www.coolphptools.com/codeigniter-smarty
+ */
+class Logwrite {
+
+    function logWrite($data="",$fileName="test",$mode = "a+") {
+        return 'Hello World';
+    }
+}
+?>
